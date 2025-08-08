0040db726d0408dae3352a78008de9c99d624317
Acorn <squirrelarbiter@gmail.com>
2025-08-07 22:21:23 -0700

    remove non-dbus code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Provides info about your current selection/clipboard on X11 through notifications.
 
-More specifically, uses python3-xlib to wait changes to the PRIMARYH selection, analyzes the selection, then uses `notify-send` to update a notification providing useful info about the selection.
+More specifically, uses `python3-xlib` to wait for changes to the PRIMARY X-selection, analyzes the selection, then sends a D-Bus message via `gdbus` to update a notification providing useful info about the selection.
 
 Useful metadata:
 * Number of characters
--- notification.py
+++ notification.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 
-import subprocess
 import re
+import subprocess
 
-import dbus
-
-# TODO: Move this to dbus
-# Example: (uint32 60,)\n
-num_re = re.compile(r' ([0-9]+)')
 
 class Notification(object):
     def __init__(self, expire_time_ms=5000):
@@ -15,38 +10,34 @@ class Notification(object):
         self.expire_time_ms = expire_time_ms
 
     def update(self, summary, body):
-        output = dbus.dbus_send_notify(self.notification_id, summary, body, self.expire_time_ms)
         try:
-            m = num_re.search(output.decode())
-            self.notification_id = int(m.group(1))
+            self.notification_id = send_dbus_notification(self.notification_id, summary, body, self.expire_time_ms)
         except Exception as e:
             print('ERROR CREATING NOTIFICATION', e)
         return
 
-    def create_notification_notify_send(self, summary, body):
-        p = subprocess.Popen(['/usr/bin/notify-send',
-                              '--app-name', 'clipmon',
-                              '--urgency', 'low',
-                              '--expire-time', str(self.expire_time_ms),
-                              '--print-id',
-                              f"{summary}",
-                              f"{body}"],
-                              stdout=subprocess.PIPE)
-        p.wait()
-        output = p.stdout.read()
-        print(output)
-        self.notification_id = output.strip()
-        if not self.notification_id:
-            print('ERROR CREATING NOTIFICATION')
 
-    def update_notify_send(self, summary, body):
-        if not self.notification_id:
-            self.create_notification(summary, body)
-        else:
-            subprocess.Popen(['/usr/bin/notify-send',
-                                '--replace-id', self.notification_id,
-                                f"{summary}",
-                                f"{body}"]).wait()
+# Example command output: (uint32 60,)\n
+num_re = re.compile(r' ([0-9]+)')
+
+def send_dbus_notification(replaces_id, summary, body, expire_timeout):
+    output = subprocess.check_output([
+        'gdbus', 'call', '--session',
+        '--dest=org.freedesktop.Notifications',
+        '--object-path=/org/freedesktop/Notifications',
+        '--method=org.freedesktop.Notifications.Notify',
+        'clipmon',             # app name
+        f'{replaces_id}',      # replaces_id
+        'gtk-dialog-info',     # app_icon
+        summary,               # summary
+        body,                  # body
+        '[]',                  # actions
+        '{}',                  # hints
+        str(expire_timeout),   # expire_timeout
+    ])
+    m = num_re.search(output.decode())
+    return int(m.group(1))
+
 
 
 if __name__ == '__main__':
